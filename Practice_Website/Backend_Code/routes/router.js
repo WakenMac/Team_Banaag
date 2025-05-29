06d02ea0913b83b64d61ddf4fdda4ccbba1d941f
Waken Maclang <maclangw26@gmail.com>
2025-05-29 16:01:06 +0800

    Removed the practice website

--- Practice_Website/Backend_Code/routes/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dbhandler = require('../controllers/mainHandler.js');
-
-// Define authentication routes
-
-router.get('/admins/all', dbhandler.getAllAdmins);
-
-module.exports = router; // Exports the router to become usable by server.js
